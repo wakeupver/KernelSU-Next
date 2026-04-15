e399dfb81b18093840c5d11678b00800666b96b8
Verdial <99362544+wakeupver@users.noreply.github.com>
2026-04-16 01:23:38 +0700

    Update kernel_compat.c

--- kernel/compat/kernel_compat.c
+++ kernel/compat/kernel_compat.c
@@ -168,7 +168,7 @@ void *ksu_compat_kvrealloc(const void *p, size_t oldsize, size_t newsize,
 	if (!newp)
 		return NULL;
 	memcpy(newp, p, oldsize);
-	kvfree((void *)p);
+	kvfree(p);
 	return newp;
 }
 #endif
